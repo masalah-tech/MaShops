5e52fa0c26affacb04c5e3edde09b04513875eea
Mazen Salah <masalah.tech@gmail.com>
2023-12-27 01:58:22 +0300

    #

---
+++ MaShops/Areas/ControlPanel/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using MaShops.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaShops.Areas.ControlPanel.Controllers
+{
+    [Area("ControlPanel")]
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products =
+                _context.Products
+                .Include(p => p.Store)
+                .Include(p => p.Category)
+                .ToList();
+
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product =
+                _context.Products
+                .Where(p => p.Id == id)
+                .Include(p => p.Category)
+                .Include(p => p.Store)
+                .FirstOrDefault();
+
+            return View(product);
+        }
+    }
+}
---
+++ MaShops/Areas/ControlPanel/Controllers/StoreController.cs
@@ -0,0 +1,37 @@
+﻿using MaShops.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaShops.Areas.ControlPanel.Controllers
+{
+    [Area("ControlPanel")]
+    public class StoreController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public StoreController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var stores =
+                _context.Stores
+                .Include(s => s.Owner)
+                .ToList();
+
+            return View(stores);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var store =
+                _context.Stores
+                .Where(s => s.Id == id)
+                .Include(s => s.Owner)
+                .FirstOrDefault();
+
+            return View(store);
+        }
+    }
+}
