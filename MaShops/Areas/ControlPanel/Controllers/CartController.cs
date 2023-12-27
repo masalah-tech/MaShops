7eb45ea415108ea7c3a21b78f70c6bd4b3eb78d6
Mazen Salah <masalah.tech@gmail.com>
2023-12-27 22:08:20 +0300

    #

---
+++ MaShops/Areas/ControlPanel/Controllers/CartController.cs
@@ -0,0 +1,58 @@
+﻿using MaShops.Data;
+using MaShops.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaShops.Areas.ControlPanel.Controllers
+{
+    [Area("ControlPanel")]
+    public class CartController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CartController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var carts = 
+                _context.ProductsCarts
+                .Include(ps => ps.Product)
+                .Include(ps => ps.Cart)
+                .Include(ps => ps.Cart.Customer)
+                .ToList();
+
+            return View(carts);
+        }
+
+        public IActionResult CartProducts(int cartId)
+        {
+            var productsCarts =
+                _context.ProductsCarts
+                .ToList();
+
+            var products =
+                _context.Products
+                .Include(p => p.Store)
+                .Include(p => p.Category)
+                .ToList();
+
+            var cartProducts = new List<Product>();
+
+            foreach (var product in products)
+            {
+                foreach (var productCart in productsCarts)
+                {
+                    if (productCart.ProductId == product.Id
+                        && productCart.CartId == cartId)
+                    {
+                        cartProducts.Add(product);
+                    }
+                }
+            }
+
+            return View(cartProducts);
+        }
+    }
+}
