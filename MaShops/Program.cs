b9d3eaea8f8a1925c5e4f4e873e6da0826aae445
Mazen Salah <masalah.tech@gmail.com>
2024-07-20 12:05:27 +0300

    Auto apply migrations on project run

--- MaShops/Program.cs
+++ MaShops/Program.cs
@@ -33,4 +33,20 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{area=Seller}/{controller=Home}/{action=Index}/{id?}");
 
+ApplyMigration();
+
 app.Run();
+
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
